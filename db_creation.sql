fbdd6862af16ce0c65821d31600e526f313bb11f
Selkis0 <selkis@esna.bzh>
2025-03-19 21:26:52 +0100

    Modification des requêtes de création de la base de données

--- db_creation.sql
+++ db_creation.sql
@@ -7,23 +7,57 @@ CREATE TABLE IF NOT EXISTS Type_alert (
     label VARCHAR(100)
 );
 
+CREATE TABLE IF NOT EXISTS Type_sky (
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    label VARCHAR(100)
+);
+
 CREATE TABLE IF NOT EXISTS Users (
     id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     email VARCHAR(255),
     password VARCHAR(255),
     city VARCHAR(100),
-    region VARCHAR(100)
 );
 
 CREATE TABLE IF NOT EXISTS Alerts (
     id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    fk_type INT NOT NULL,
     region VARCHAR(100),
     description VARCHAR(255),
     active INT,
     date_alert DATE,
     FOREIGN KEY (fk_type) REFERENCES Type_alert(id)
 );
+
+CREATE TABLE IF NOT EXISTS Region (
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    label VARCHAR(100)
+)
+
+CREATE TABLE IF NOT EXISTS Department (
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    label VARCHAR(100),
+    FOREIGN KEY (region) REFERENCES Region(id)
+)
+
+CREATE TABLE IF NOT EXISTS Cities (
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    label VARCHAR(100),
+    FOREIGN KEY (department) REFERENCES Department(id)
+)
+
+CREATE TABLE IF NOT EXISTS Weather (
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    date_weather DATE,
+    temperature_min FLOAT,
+    temperature_max FLOAT,
+    pressure FLOAT,
+    humidity FLOAT,
+    wind_speed FLOAT,
+    sunrise TIME,
+    sunset TIME,
+    FOREIGN KEY (fk_type) REFERENCES Type_sky(id)
+    FOREIGN KEY (city) REFERENCES Cities(id)
+)
 
 INSERT INTO Type_alert (label) VALUES 
 ('Tempête'),
